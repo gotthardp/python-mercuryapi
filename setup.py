ed2ceb316faf13196b805d69d95938677143cfe7
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2019-02-19 15:43:13 +0100

    Issue #50: Ability to upload new release to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 # python3 setup.py build
-
+# python3 setup.py sdist upload
 from setuptools import setup, Extension
-setup(name="mercuryapi", version="0.4",
+setup(name="python-mercuryapi", version="0.4.1",
+      author="Petr Gotthard",
+      author_email="petr.gotthard@centrum.cz",
+      description="Python wrapper for the ThingMagic Mercury API",
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      url="https://github.com/gotthardp/python-mercuryapi",
+      classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta"
+      ],
       ext_modules=[Extension("mercury",
                              sources=["mercury.c"],
                              libraries=["mercuryapi", "ltkc", "ltkctm"],
