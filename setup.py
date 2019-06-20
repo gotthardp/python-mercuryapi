0276ad4b7758d7f06b589ac42a21af2c660c0a45
Petr Gotthard <petr.gotthard@konicaminolta.cz>
2019-06-20 14:57:32 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class my_build(build):
         os.system("make mercuryapi")
         build.run(self)
 
-setup(name="python-mercuryapi", version="0.5.0",
+setup(name="python-mercuryapi", version="0.5.1",
       author="Petr Gotthard",
       author_email="petr.gotthard@centrum.cz",
       description="Python wrapper for the ThingMagic Mercury API",
